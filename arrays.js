b9e0708e2aabf3969bdf126125ded54a574536a5
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-04-18 12:50:44 +0300

    commit

---
+++ arrays.js
@@ -0,0 +1,11 @@
+
+groceryfruits= ["mango", "papaya", "avocado","pineapple","orange","banana","grapes"];
+grocerystocks = [10,29,19,15,14,25,24];
+
+function addItems(fruit,stock){
+   
+
+}
+groceryfruits.push(fruit);
+grocerystocks.push(stock);
+console.log(addItems("watermelon",10));
